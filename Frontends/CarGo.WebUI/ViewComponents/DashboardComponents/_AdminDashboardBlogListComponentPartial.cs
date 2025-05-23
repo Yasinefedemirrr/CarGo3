a5a7b2a56cf0d490f14fe7d9a9104b2627b1be61
90545 <yasinefedemir9@gmail.com>
2025-05-23 04:14:31 +0300

    Admin Dashboard İşlemi

---
+++ Frontends/CarGo.WebUI/ViewComponents/DashboardComponents/_AdminDashboardBlogListComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using CarGo.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarGo.WebUI.ViewComponents.DashboardComponents
+{
+    public class _AdminDashboardBlogListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _AdminDashboardBlogListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7266/api/Blogs/GetAllBlogsWithAuthorList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
