cdb6fe2fb11ba7abd1d2d59e7126018cb1c15317
90545 <yasinefedemir9@gmail.com>
2025-05-24 19:36:05 +0300

    AppUser Query ve Result İşlemleri

--- Core/CarGo.Application/Features/Mediator/Queries/AppUserQueries/GetCheckAppUserQuery.cs
+++ Core/CarGo.Application/Features/Mediator/Queries/AppUserQueries/GetCheckAppUserQuery.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 using CarGo.Application.Features.Mediator.Results.AppUserResults;
 using MediatR;
 
-namespace CarGo.Application.Features.Mediator.Queries.AppUserQueries
-{
-    public class GetCheckAppUserQuery : IRequest<GetCheckAppUserQueryResult>
-    {
+ namespace CarGo.Application.Features.Mediator.Queries.AppUserQueries
+ {
+     public class GetCheckAppUserQuery : IRequest<GetCheckAppUserQueryResult>
+     {
         public string Username { get; set; }
         public string Password { get; set; }
     }
-}
+ }
