f33f1d49aef4dfc6fd8a11b26c69b2609604439b
90545 <yasinefedemir9@gmail.com>
2025-05-24 19:51:31 +0300

    JwtTokenDefault İşlemleri

---
+++ Core/CarGo.Application/Tools/JwtTokenGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using CarGo.Application.Dtos;
+using CarGo.Application.Features.Mediator.Results.AppUserResults;
+
+namespace CarGo.Application.Tools
+{
+    public class JwtTokenGenerator
+    {
+       
+    }
+}
