8b310f853355457211b6c9267878c920534b4df1
90545 <yasinefedemir9@gmail.com>
2025-05-24 19:46:24 +0300

    AppUser Handler İşlemler ve Token Dtos İşlemleri

---
+++ infrastructure/CarGo.Persistence/Repositories/AppUserRepositories/AppUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using CarGo.Application.interfaces.AppUserInterfaces;
+using Cargo.Domain.Entities;
+using CarGo.Persistence.Context;
+
+namespace CarGo.Persistence.Repositories.AppUserRepositories
+{
+    public class AppUserRepository : IAppUserRepository
+    {
+        private readonly CarGoContext _context;
+        public AppUserRepository(CarGoContext context)
+        {
+            _context = context;
+        }
+        public Task<List<AppUser>> GetByFilterAsync(Expression<Func<AppUser, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
